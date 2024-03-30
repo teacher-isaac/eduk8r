3b4c6ef4618b601ce493e621cb9671ddeb5ea78d
Teacher Isaac <teacher@eduk8.us>
2024-03-30 13:55:41 +0700

    use docker run options to mount bind in DEV or as RO volumes in PROD

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 FROM php:fpm-alpine3.18
-RUN apk --no-cache add nginx php-fpm
+RUN apk --no-cache add nginx php-fpm php-pecl-xdebug
 COPY docker/nginx.conf /etc/nginx/http.d/default.conf
 COPY docker/php-fpm-overrides.conf /usr/local/etc/php-fpm.d/~overrides.conf
-WORKDIR /var/www/eduk8r
-COPY www/ www/
+# bind mount points in DEV
+# or read only vlumes in PROD
+# specified as ducker run options
+#WORKDIR /var/www/eduk8r
+#COPY include/ include/
+#COPY www/ www/
 CMD php-fpm -D; nginx -g "daemon off;"
 EXPOSE 8080
--- docker/build-run.sh
+++ docker/build-run.sh
@@ -1,7 +1,34 @@
 #!/usr/bin/bash -x
+#
+# usage: build-run.sh [DEV|PROD]
 
-IMG_NAME=eduk8r-app
-IMG_RUN_NAME=eduk8r-runtime
+SCRIPT=$(realpath "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+PRG_PATH=$(dirname "$SCRIPT_PATH")
+echo "PRG_PATH: $PRG_PATH"
+IMAGE_NAME=eduk8r-app
+CONTAINER_NAME=eduk8r-runtime
+TARGET=PROD
+
+if [ ! -z "$1" ]
+then
+	TARGET="$1"
+fi
 
 docker build -t $IMG_NAME .
-docker run -dp 127.0.0.1:8080:8080 --name $IMG_RUN_NAME $IMG_NAME
+if [ "$TARGET" == "DEV" ]
+then
+	docker run \
+		--detach=true \
+		--publish 127.0.0.1:8080:8080 \
+		--mount "type=bind,source=$PRG_PATH/include,destination=/var/www/eduk8r/include" \
+		--mount "type=bind,source=$PRG_PATH/www,destination=/var/www/eduk8r/www" \
+		--name $CONTAINER_NAME $IMAGE_NAME
+else
+	docker run \
+		--detach=true \
+		--publish 127.0.0.1:8080:8080 \
+		--volume include:/var/www/eduk8r/include:ro \
+		--volume www:/var/www/eduk8r/www:ro \
+		--name $CONTAINER_NAME $IMAGE_NAME
+fi
